2613c65e74fcef0830fd203cce39c3be2e1c8daa
ebquhf <barna211@vipmail.hu>
2022-02-19 13:46:03 +0100

    implements processEvent in MachineInterpreter.java

--- src/main/MachineInterpreter.java
+++ src/main/MachineInterpreter.java
@@ -2,13 +2,15 @@ package main;
 
 import main.metamodel.Machine;
 import main.metamodel.State;
+import main.metamodel.Transition;
 
 public class MachineInterpreter {
 	Machine machine;
 	State current;
+
 	public void run(Machine m) {
-		machine=m;
-		current=m.getInitialState();
+		machine = m;
+		current = m.getInitialState();
 	}
 
 	public State getCurrentState() {
@@ -16,8 +18,10 @@ public class MachineInterpreter {
 	}
 
 	public void processEvent(String string) {
-		// TODO Auto-generated method stub
-		
+		Transition temp = current.getTransitionByEvent(string);
+		if (temp != null) {
+			current = temp.getTarget();
+		}
 	}
 
 	public int getInteger(String string) {
